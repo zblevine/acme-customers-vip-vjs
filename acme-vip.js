a71da81b0c8f4d12ff8d7533624610aa630e0130
Jesen Tanadi <mail@jesentanadi.com>
2019-07-18 20:24:15 -0400

    add functionality

--- acme-vip.js
+++ acme-vip.js
@@ -1,9 +1,41 @@
 const customers = [];
 
-const nameBox = Document.querySelector('#name')
-const emailBox = Document.querySelector('#email')
-const vipBox = Document.querySelector('#is-vip')
+const nameBox = document.querySelector('#name')
+const emailBox = document.querySelector('#email')
+const vipBox = document.querySelector('#is-vip')
+const vipCounts = document.querySelector('#count-vips');
+const vips = document.querySelector("#vips");
 
-Document.querySelector('#create').addEventListener('click', ev => {
-   customers.push({name: nameBox.value, email: emailBox.value, isVIP: vipBox.value}) 
+render();
+
+document.querySelector('#create').addEventListener('click', ev => {
+  let id = Math.floor(Math.random() * 100);
+  while(customers.find(customer => {
+    return customer.id === id
+  })) {
+    id = Math.floor(Math.random() * 100);
+  }
+
+   customers.push({id: id, name: nameBox.value, email: emailBox.value, isVIP: vipBox.checked})
+
+  render();
+  console.log(customers);
 });
+
+function render() {
+  vipCounts.innerHTML = `<p>${customers.length} VIPs</p>`
+
+  vips.innerHTML = "";
+  customers.forEach(customer => {
+    const li = document.createElement("li");
+
+    if (customer.isVIP) {
+      li.classList.add("vip");
+    }
+
+    li.innerHTML = `${customer.name} (${customer.email})`;
+    vips.appendChild(li);
+  })
+}
+
+
